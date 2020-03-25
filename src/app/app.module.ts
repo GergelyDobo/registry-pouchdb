0bbe0d89fb0d7556942b9e9de34d0fdbfb39606e
Dobó Gergely <dgergely98@windowslive.com>
2020-03-25 20:49:56 +0100

    Implement HeaderComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { ProductsComponent } from './components/products/products.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-	declarations: [AppComponent, ProductsComponent],
+	declarations: [AppComponent, ProductsComponent, HeaderComponent],
 	imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
 	providers: [],
 	bootstrap: [AppComponent]
